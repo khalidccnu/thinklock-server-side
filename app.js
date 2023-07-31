561fbb2e00f5e43da0b7099f486789b1d0273861
S.M. Khalid Mahmud <khalidccnu@gmail.com>
2023-07-31 06:20:13 +0600

    vercel config fixed

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const port = process.env.PORT || 5000;
 
 const corsOptions = {
   origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
   optionSuccessStatus: 200,
 };
